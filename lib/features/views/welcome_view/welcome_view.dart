5e0f4cb869fcc1ad7600bdf94ff383dc28c90d59
hashem <hashemhassen0@gmail.com>
2024-03-08 19:16:18 +0200

    create the main pic

--- lib/features/views/welcome_view/welcome_view.dart
+++ lib/features/views/welcome_view/welcome_view.dart
@@ -5,10 +5,49 @@ class WelcomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: Center(
-        child: Text('Welcome'),
-      )
-    );
+        body: SingleChildScrollView(
+      child: Column(
+        children: [
+          Stack(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(28),
+                  child: Image.asset(
+                    'assets/images/house_1.jpg',
+                    height: height * 0.6,
+                    width: width,
+                    fit: BoxFit.fitWidth,
+                  ),
+                ),
+              ),
+              const Positioned(
+                top: 30,
+                left: 30,
+                right: 30,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.home_filled, size: 40, color: Colors.white),
+                    SizedBox(width: 10),
+                    Text(
+                      'SoHouser',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    ));
   }
 }
