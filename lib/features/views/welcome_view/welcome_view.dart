e6b94969bd13eff30ea9f0c902126a8e8dcb7d96
hashem <hashemhassen0@gmail.com>
2024-03-08 19:45:44 +0200

    Welcome View UI Done

--- lib/features/views/welcome_view/welcome_view.dart
+++ lib/features/views/welcome_view/welcome_view.dart
@@ -10,6 +10,7 @@ class WelcomeView extends StatelessWidget {
     return Scaffold(
         body: SingleChildScrollView(
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Stack(
             children: [
@@ -46,6 +47,59 @@ class WelcomeView extends StatelessWidget {
               ),
             ],
           ),
+          SizedBox(height: width * 0.07),
+          const Text(
+            'Discover Dream House from Smartphone',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: width * 0.02),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 28.0),
+            child: Text(
+              'Lorem ipsum dolor sit amet' * 3,
+              textAlign: TextAlign.center,
+              style: const TextStyle(fontSize: 16, color: Colors.black45),
+            ),
+          ),
+          SizedBox(height: width * 0.07),
+          ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              shape: const StadiumBorder(),
+              backgroundColor: Colors.black,
+              elevation: 8,
+              minimumSize: Size(width * 0.8, height * 0.06),
+            ),
+            child: const Text(
+              'Register Now',
+              style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
+          ),
+          SizedBox(height: width * 0.02),
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            const Text(
+              'Already have an account?',
+            ),
+            TextButton(
+              onPressed: () {},
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.blue,
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              ),
+              child: const Text(
+                'Login',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue,
+                ),
+              ),
+            ),
+          ]),
         ],
       ),
     ));
