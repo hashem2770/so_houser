72414231436c0206693ab85ff081cd498182d30b
hashem <hashemhassen0@gmail.com>
2024-03-11 23:13:05 +0200

    create property model

--- lib/features/views/home_view/home_view.dart
+++ lib/features/views/home_view/home_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:so_houser/features/views/home_view/widgets/advertise_card.dart';
 import 'package:so_houser/features/views/home_view/widgets/app_bar_button.dart';
 import 'package:so_houser/features/views/home_view/widgets/featured_advertise.dart';
+import 'package:so_houser/models/property_model.dart';
 
 import '../../../core/components/custom_search_field.dart';
 
@@ -60,49 +61,39 @@ class HomeView extends StatelessWidget {
                   AppBarButton(icon: const Icon(Icons.tune), onPressed: () {}),
                 ],
               ),
-          
+
               SizedBox(height: height * 0.04),
-          
+
               /// search bar
               const CustomSearchField(hintText: 'Search for properties'),
               SizedBox(height: height * 0.04),
-          
+
               /// featured properties advertise
               FeaturedAdvertise(
                   height: height,
                   width: width,
                   imagePath: 'assets/images/house_2.jpg',
                   offer: '*Get 5% off on your first booking'),
-          
+
               SizedBox(height: height * 0.04),
-          
+
               /// properties offers
-              AdvertiseCard(
-                height: height,
-                width: width,
-                imagePath: 'assets/images/house_4.jpg',
-                location: 'Sohag, Egypt',
-                placeName: 'Whitespace House',
-                price: 1199,
-              ),
-              SizedBox(height: height * 0.005),
-              AdvertiseCard(
-                height: height,
-                width: width,
-                imagePath: 'assets/images/house_3.jpg',
-                location: 'Qena, Egypt',
-                placeName: 'Upper moon House',
-                price: 850,
-              ),
-              SizedBox(height: height * 0.005),
-              AdvertiseCard(
-                height: height,
-                width: width,
-                imagePath: 'assets/images/house_1.jpg',
-                location: 'Giza, Egypt',
-                placeName: 'Eve Heaven House',
-                price: 400,
-              ),
+              ListView.separated(
+                itemBuilder: (context, index) {
+                  final property = PropertyModel.properties[index];
+                  return AdvertiseCard(
+                    height: height,
+                    width: width,
+                    imagePath: property.imageUrl,
+                    location: property.location,
+                    placeName: property.title,
+                    price: property.price,
+                  );
+                },
+                separatorBuilder: (context, index) =>
+                    SizedBox(height: height * 0.02),
+                itemCount: PropertyModel.properties.length,
+              )
             ],
           ),
         ),
@@ -110,4 +101,3 @@ class HomeView extends StatelessWidget {
     );
   }
 }
-
