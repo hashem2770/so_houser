bc0c43a15906fbdd4b6f87d51f25b96b45fb61a7
hashem <hashemhassen0@gmail.com>
2024-03-11 23:59:58 +0200

    refactor property model

--- lib/models/property_model.dart
+++ lib/models/property_model.dart
@@ -3,12 +3,18 @@ class PropertyModel {
   String imageUrl;
   int price;
   String location;
+  int beds;
+  int baths;
+  int area;
 
   PropertyModel({
     required this.title,
     required this.imageUrl,
     required this.price,
     required this.location,
+    this.beds = 0,
+    this.baths = 0,
+    this.area = 0,
   });
   static List<PropertyModel> properties = [
     PropertyModel(
@@ -16,12 +22,18 @@ class PropertyModel {
       imageUrl: 'assets/images/house_1.jpg',
       price: 400,
       location: 'Giza, Egypt',
+      area: 1200,
+      beds: 3,
+      baths: 2,
     ),
     PropertyModel(
       title: 'Whitespace House',
       imageUrl: 'assets/images/house_2.jpg',
       price: 1199,
       location: 'Sohag, Egypt',
+      area: 1200,
+      beds: 3,
+      baths: 2,
 
     ),
     PropertyModel(
@@ -29,6 +41,9 @@ class PropertyModel {
       imageUrl: 'assets/images/house_3.jpg',
       price: 400,
       location: 'Giza, Egypt',
+      area: 1200,
+      beds: 3,
+      baths: 2,
 
     ),
     PropertyModel(
@@ -36,12 +51,18 @@ class PropertyModel {
       imageUrl: 'assets/images/house_4.jpg',
       price: 450,
       location: 'Sohag, Egypt',
+      area: 1200,
+      beds: 3,
+      baths: 2,
     ),
     PropertyModel(
       title: 'Whitespace House',
       imageUrl: 'assets/images/house_5.jpg',
       price: 555,
       location: 'Sohag, Egypt',
+      area: 1200,
+      beds: 3,
+      baths: 2,
     ),
   ];
 }
